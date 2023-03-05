dc5522a98f66617469b12e5d7ab5e9d194c7b585
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-03-04 19:27:30 -0500

    implemented enums inside of colorSchemeAndPaletteStore.js file to improve type checking

--- src/lib/stores/colorSchemeAndPaletteStore.ts
+++ src/lib/stores/colorSchemeAndPaletteStore.ts
@@ -1,54 +1,73 @@
 import { browser } from '$app/environment';
 import { writable, type Writable } from 'svelte/store';
 
-enum ColorScheme {
-	'light',
-	'dark'
+export enum ColorScheme {
+	light = 'light',
+	dark = 'dark'
 }
-enum themePalette {
-	'main',
-	'desert',
-	'dusk',
-	'night-sky'
+export enum ThemePalette {
+	main = 'main',
+	desert = 'desert',
+	dusk = 'dusk',
+	nightSky = 'nightSky'
 }
 
 // subscriptions always run with the initial value
-export const colorSchemeStore: Writable<ColorScheme> = writable('light');
-export const paletteStore: Writable<themePalette> = writable('main');
+export const colorSchemeStore: Writable<ColorScheme> = writable(ColorScheme.light);
+export const themePaletteStore: Writable<ThemePalette> = writable(ThemePalette.main);
 
-export function toggleColorScheme() {
-	colorSchemeStore.update((ColorScheme: ColorScheme) => {
-		return ColorScheme === ColorScheme ? 'dark' : 'light';
-	});
+export function trySetScheme(value: any) {
+	if (typeof value === 'string' && value in ColorScheme) {
+		colorSchemeStore.set(value as ColorScheme);
+		return true;
+	}
+	return false;
+}
+export function trySetPalette(value: any) {
+	if (typeof value === 'string' && value in ThemePalette) {
+		themePaletteStore.set(value as ThemePalette);
+		return true;
+	}
+	return false;
 }
 
-export function getBrowserPreferredColorScheme() {
-	return matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+export function toggleColorScheme() {
+	colorSchemeStore.update((scheme: ColorScheme) => {
+		return scheme === ColorScheme.light ? ColorScheme.dark : ColorScheme.light;
+	});
 }
 
 function initializeColorScheme() {
 	if (browser) {
-		const colorScheme = window.localStorage.getItem('NazCodeland.colorScheme');
-		colorSchemeStore.set(colorScheme ?? getBrowserPreferredColorScheme());
+		if (!trySetScheme(window.localStorage.getItem('NazCodeland.colorScheme'))) {
+			colorSchemeStore.set(getBrowserPreferredColorScheme());
+		}
 	}
 }
 function initializeThemePalette() {
 	if (browser) {
-		const themePalette = window.localStorage.getItem('NazCodeland.themePalette');
-		if (themePalette) {
-			paletteStore.set(themePalette);
-		}
+		trySetPalette(window.localStorage.getItem('NazCodeland.themePalette'));
 	}
 }
-// initialize first
+
+function getBrowserPreferredColorScheme(): ColorScheme {
+	if (browser) {
+		if (matchMedia(`(prefers-color-scheme: ${ColorScheme.dark})`).matches) {
+			return ColorScheme.dark;
+		}
+	}
+	return ColorScheme.light;
+}
+
 initializeColorScheme();
 initializeThemePalette();
 
-// then subscribe
 colorSchemeStore.subscribe((colorScheme) => {
-	if (browser) window.localStorage.setItem('NazCodeland.colorScheme', colorScheme);
+	if (browser) {
+		window.localStorage.setItem('NazCodeland.colorScheme', colorScheme);
+	}
 });
-paletteStore.subscribe((themePalette) => {
+themePaletteStore.subscribe((themePalette) => {
 	if (browser) {
 		window.localStorage.setItem('NazCodeland.themePalette', themePalette);
 	}
