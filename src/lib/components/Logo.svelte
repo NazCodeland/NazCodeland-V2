25c74a29917f9f25819385c8e03ecbf6b2d724c3
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-03-11 11:24:04 -0500

    implemented logo.svelte using svelte/sveltekit 'way'

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,34 +1,46 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
-	let logo: HTMLElement;
 	let logoText: string = 'NazCodeland';
 
-	function spanLogo(logoText: string) {
-		const logoTextArray = [...logoText];
-		return logoTextArray.map((letter) => {
-			return letter === 'N'
-				? `<a href="/" class="relative font-logoFirstL text-logoFirstL font-bold leading-logoFirstL cursor-pointer rounded-sm text-primaryColor" id="logo" aria-label="NazCodeland">
-					${letter}
-					</a>`
-				: `<span class="font-logoNotFirstL text-logoNotFirstL leading-logoNotFistL cursor-default">
-					${letter}
-					</span>`;
-		});
-	}
-
-	let spannedLogo = spanLogo(logoText);
-	spannedLogo = [
-		`${spannedLogo.slice(0, 1)}<span aria-hidden="true">${spannedLogo.slice(1).join('')}</span>`
-	];
-
-	onMount(() => {
-		logo.innerHTML = spannedLogo[0];
-	});
+	const logoTextArray = [...logoText];
 </script>
 
-<h1 class="logo flex-shrink-0 rounded-sm " bind:this={logo} aria-labelledby="logo">NazCodeland</h1>
+<!-- TODO: this should not be an H1 -->
+<h1 class="logo flex-shrink-0 rounded-sm " aria-labelledby="logo">
+	{#each logoTextArray as letter}
+		{#if letter === 'N'}
+			<!-- prettier-ignore -->
+			<a href="/" class="relative font-logoFirstL text-logoFirstL font-bold leading-logoFirstL cursor-pointer rounded-sm text-primaryColor" id="logo" aria-label="NazCodeland">
+			{letter}
+		</a>
+		{:else}
+			<span class="flex cursor-default font-logoNotFirstL text-logoNotFirstL leading-logoNotFistL">
+				{letter}
+			</span>
+		{/if}
+	{/each}
+</h1>
 
 <style>
 	/* this component contains some styles in logo.css */
+	.logo *:not([aria-hidden='true']) {
+		@apply inline-block
+		px-[0.05rem]
+		tracking-[clamp(0.15rem,_calc(-0.07rem_+_0.36vw),_0.5rem)]
+		shadow-logo
+		outline
+		outline-[0.1px]
+		transition-transform
+		duration-200;
+	}
+
+	.logo *:not([aria-hidden='true']):hover {
+		@apply -translate-y-5
+		rotate-[20deg]
+		scale-[2]
+		uppercase;
+	}
+
+	.logo > span :nth-child(1) {
+		@apply ml-[0.1rem];
+	}
 </style>
