788565b1732aed0804f311afe8dd4f641d0a0237
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-03-15 19:00:17 -0400

    style: added added hover and visible styles to buttons

--- src/lib/components/skillCard.svelte
+++ src/lib/components/skillCard.svelte
@@ -4,9 +4,14 @@
 
 <a
 	href="/"
-	class="flex-auto basis-[30ch] rounded-lg p-4 shadow-rest outline outline-primaryColor transition-transform delay-[0s] duration-[0.15s]
-	ease-in-out hover:scale-[1.01] hover:cursor-pointer hover:shadow-hover 
-	active:scale-active active:shadow-active group-data-[themepalette=main]:outline-none dark:bg-slate-50/12 dark:hover:bg-slate-50/20 dark:active:bg-slate-50/12">
+	class="flex-auto basis-[30ch] rounded-lg p-4 shadow-rest outline outline-primaryColor 
+	transition-transform delay-[0s] duration-[0.15s] ease-in-out hover:scale-[1.01]
+	hover:cursor-pointer hover:shadow-hover hover:outline-[canvasText] focus-visible:outline-[canvasText] 
+	active:scale-active
+	active:shadow-active 
+	group-data-[themepalette=main]:outline-0
+	group-data-[themepalette=main]:focus-visible:outline-2  
+	dark:bg-slate-50/12 dark:hover:bg-slate-50/20 dark:active:bg-slate-50/12">
 	<header data-theme="warning" class="mb-8-16 flex items-end gap-4">
 		<slot name="icon" />
 		<h3 class="mb-0">
