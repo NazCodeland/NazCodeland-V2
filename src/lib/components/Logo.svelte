03795c45618e7b00b6cd5ca50d7ac959618c042b
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-03-17 15:12:34 -0400

    style: fixed focus-visible outline on first letter of Logo

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -10,14 +10,24 @@
 		{#if letter === 'N'}
 			<a
 				href="/"
-				class="relative mr-[2px] inline-block cursor-pointer rounded-sm px-[0.05rem] font-logoFirstL text-logoFirstL font-bold leading-logoFirstL tracking-[clamp(0.15rem,_calc(-0.07rem_+_0.36vw),_0.5rem)] text-logoFirstLClr shadow-logo outline outline-primaryColor transition-transform duration-200 hover:-translate-y-5 hover:rotate-[20deg] hover:scale-[2] hover:uppercase dark:outline dark:outline-[0.1px] dark:outline-slate-50/20"
+				class="relative mr-[2px] inline-block cursor-pointer rounded-sm px-[0.05rem] 
+				font-logoFirstL text-logoFirstL font-bold leading-logoFirstL 
+				tracking-[clamp(0.15rem,_calc(-0.07rem_+_0.36vw),_0.5rem)] text-logoFirstLClr 
+				shadow-logo outline outline-primaryColor transition-transform duration-200 
+				hover:-translate-y-5 hover:rotate-[20deg] hover:scale-[2] hover:uppercase 
+				focus-visible:outline-[canvasText]"
 				id="logo"
 				aria-label="NazCodeland">
 				{letter}
 			</a>
 		{:else}
 			<span
-				class="inline-block cursor-default px-[0.05rem] font-logoNotFirstL text-logoNotFirstL leading-logoNotFistL tracking-[clamp(0.15rem,_calc(-0.07rem_+_0.36vw),_0.5rem)] shadow-logo transition-transform duration-200 hover:-translate-y-5 hover:rotate-[20deg] hover:scale-[2] hover:uppercase dark:outline dark:outline-[0.1px] dark:outline-slate-50/20">
+				class="inline-block cursor-default px-[0.05rem] font-logoNotFirstL 
+				text-logoNotFirstL leading-logoNotFistL 
+				tracking-[clamp(0.15rem,_calc(-0.07rem_+_0.36vw),_0.5rem)] 
+				shadow-logo transition-transform duration-200 hover:-translate-y-5 
+				hover:rotate-[20deg] hover:scale-[2] hover:uppercase dark:outline 
+				dark:outline-[0.1px] dark:outline-slate-50/20">
 				{letter}
 			</span>
 		{/if}
