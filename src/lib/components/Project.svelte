f1741cc6f8c70a2f7d6b5079ccb6a3f6de402a8f
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-04-04 19:27:39 -0400

    markup: & style: added details to project images

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -9,7 +9,8 @@
 	export let objectFit: string = 'cover';
 </script>
 
-<figure class="relative overflow-hidden rounded-xl">
+<figure
+	class="relative overflow-hidden rounded-2xl border border-slate-300 p-1 dark:border-slate-600">
 	<a href="/">
 		<img
 			style="inline-size: {inlineSize}; block-size:{blockSize}; object-fit:{objectFit}"
@@ -17,6 +18,24 @@
 			src="/images/{imageName}"
 			alt="a cute dog" />
 	</a>
+	<figcaption class="ml-2 text-sm">
+		<span class="absolute top-3 rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy">
+			<i>Project:</i>
+			{project}
+		</span>
+		<span class="absolute bottom-[68px] rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy">
+			<i>Roles:</i>
+			{roles}
+		</span>
+		<span class="absolute bottom-10 rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy">
+			<i>Tools:</i>
+			{tools}
+		</span>
+		<span class="absolute bottom-3 rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy">
+			<i>Duration:</i>
+			{duration}
+		</span>
+	</figcaption>
 </figure>
 
 <style>
