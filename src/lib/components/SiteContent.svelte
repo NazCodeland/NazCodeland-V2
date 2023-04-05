7e54d7b5cacfbcacb02c048354b66fd897f9dfc5
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-04-05 04:42:17 -0400

    bug: fix colorSchemeToggle logic not working inside project pages

--- src/lib/components/SiteContent.svelte
+++ src/lib/components/SiteContent.svelte
@@ -1,9 +1,11 @@
 <script>
+	import setAttributeOnDocumentAction from '$lib/actions/setAttributeOnDocumentAction';
 </script>
 
 <div
 	class="flex flex-col gap-96-132 rounded-2xl bg-secondaryColor p-16-40 font-body
-          text-fluid-3 leading-7 text-bodyCopy md:leading-8">
+          text-fluid-3 leading-7 text-bodyCopy md:leading-8"
+	use:setAttributeOnDocumentAction>
 	<!--  -->
 	<slot name="header" />
 
