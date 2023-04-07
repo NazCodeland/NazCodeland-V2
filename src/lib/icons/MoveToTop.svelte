4b1c9de867e05b3f6044746d5a10428002c3386f
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-04-07 12:22:25 -0400

    style: changed fixed positioning

--- src/lib/icons/MoveToTop.svelte
+++ src/lib/icons/MoveToTop.svelte
@@ -4,7 +4,7 @@
 
 <!-- TODO: how can I prevent this component from going into the footer area -->
 <div
-	class="fixed right-[45%] bottom-[6vh] z-10 rounded-full border border-primaryColor bg-secondaryColor shadow-rest hover:shadow-hover active:shadow-active">
+	class="fixed left-[45vw] bottom-[6vh] z-10 rounded-full border border-primaryColor bg-secondaryColor shadow-rest hover:shadow-hover active:shadow-active sm:left-[48vw]">
 	<a
 		tabindex="0"
 		class="grid h-[40px] w-[40px] place-items-center rounded-full text-3xl text-primaryColor dark:bg-slate-1/12 dark:hover:bg-slate-50/20 dark:active:bg-slate-1/12"
