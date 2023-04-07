7b8318b1b2eaba6cb1f3862178deb2dcf6e4d4c1
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-04-06 23:03:53 -0400

    chore: created mutitple icons inside of /icons directory

---
+++ src/lib/icons/MoveToTop.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Icon from '$lib/components/Icon.svelte';
+</script>
+
+<!-- TODO: how can I prevent this component from going into the footer area -->
+<div
+	class="fixed right-[45%] bottom-[6vh] z-10 rounded-full border border-primaryColor bg-secondaryColor shadow-rest hover:shadow-hover active:shadow-active">
+	<a
+		tabindex="0"
+		class="grid h-[40px] w-[40px] place-items-center rounded-full text-3xl text-primaryColor dark:bg-slate-1/12 dark:hover:bg-slate-50/20 dark:active:bg-slate-1/12"
+		href="#mainContent">
+		<Icon iconName="ChevronLineUp" />
+	</a>
+</div>
+
+<style>
+</style>
