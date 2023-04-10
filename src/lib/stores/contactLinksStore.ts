cc18fffe5cb95593ee954b1f1c747d9d554a18ae
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-04-10 01:54:04 -0400

    chore: create contactLinks store

---
+++ src/lib/stores/contactLinksStore.ts
@@ -0,0 +1,17 @@
+import { type Writable, writable } from 'svelte/store';
+
+export const contactLinksStore: Writable<boolean> = writable(false);
+
+export function showContactLinks() {
+	contactLinksStore.update((value) => {
+		value = !value;
+
+		setTimeout(() => {
+			contactLinksStore.update((value) => {
+				return (value = !value);
+			});
+		}, 800);
+
+		return value;
+	});
+}
