ab91b29a5f523de814d57f1f440dba9f11ed17c8
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-04-19 15:48:36 -0400

    fix: capitalize BlogPost.svelte, SkillCard.svelte, LazyLoad.svelte components

---
+++ src/lib/components/BlogPost.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let src: string = 'https://picsum.photos/800/200';
+	export let href: string;
+	export let title: string;
+	export let description: string;
+	export let published: string;
+	export let updated: string = '';
+	export let tags: string[];
+</script>
+
+<article class="flex w-full flex-col gap-3">
+	<img class="img aspect-[800/200]" {src} alt="a cute dog" loading="lazy" />
+
+	<div class="">
+		<h2>
+			<a class="decoration-primaryColor hover:underline" href="/blog/{href}">
+				{title}
+			</a>
+		</h2>
+
+		<div class="text-fluid-4 opacity-70">
+			<span>{published}</span>
+			<span>{updated}</span>
+		</div>
+	</div>
+
+	<p class="mb-1">{description}</p>
+
+	<div class="mt-auto flex gap-1">
+		{#each tags as tag}
+			<a class="tag" href="/">{tag.toUpperCase()}</a>
+		{/each}
+	</div>
+</article>
+
+<style>
+</style>
