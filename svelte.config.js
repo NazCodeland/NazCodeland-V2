4be7101353656717390d49cf2e64cf00fcc14112
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-04-19 15:30:42 -0400

    fix: configure svelte.config.js to be able to deploy to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -25,6 +25,13 @@ const config = {
 				toggleKeyCombo: 'control-alt'
 			}
 		}
+	},
+
+	// this build option is so vercel can locate the Giscus component
+	rollupOptions: {
+		external: [
+			'C:/Users/o0/source/projects/mine/NazCodeLandSvelteKit/node_modules/@giscus/svelte/package/Giscus.svelte'
+		]
 	}
 };
 
