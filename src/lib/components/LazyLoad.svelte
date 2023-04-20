fb9b50bad8a2bf5d96427a4df0b74586b20f07a4
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-04-20 12:52:17 -0400

    fix: rename blogPost.svelte, lazyLoad.svelte and skillCard.svelte components to start with capital letters

---
+++ src/lib/components/LazyLoad.svelte
@@ -0,0 +1,32 @@
+<!-- CREDIT: https://lihautan.com/notes/svelte-lazy-load/#lazy-loading-svelte-component -->
+<!-- https://www.youtube.com/watch?v=eXM0i6qgJyY&t=1676s -->
+
+<script lang="ts">
+	import viewport from '$lib/actions/viewportAction';
+
+	export let component: Function;
+	let componentPromise: Promise<any>;
+	let inViewport = false;
+	let checkViewport = true;
+</script>
+
+<!-- for some reason, `on:enteringViewport` doesn't activat eafter the first time.
+		For example, if you load the site, then go to another page,
+		and go back to the home page, and scroll down to the "about me" section,
+		that contains the <LazyLoad /> component, `on:enteringViewport` doesn't activate
+-->
+{#if checkViewport}
+	<div
+		use:viewport
+		on:enteringViewport={() => {
+			componentPromise = component();
+			inViewport = true;
+			checkViewport = false;
+		}} />
+{/if}
+
+{#if inViewport}
+	{#await componentPromise then { default: Component }}
+		<slot {Component} />
+	{/await}
+{/if}
---
+++ src/lib/components/SkillCard.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import loadThemeBasedIcon from '$lib/actions/loadThemeBasedIconAction';
+	import arrowDown from '$lib/icons/themeBased/arrowDown/arrowDownMain.svg';
+
+	export let title: string;
+
+	let blockSize: number;
+	let open: Boolean = false;
+
+	function expandCard() {
+		open = !open;
+	}
+	function scrollIntoView({ target }) {
+		if (open) return;
+
+		let selectedArticle = target.closest('article');
+		selectedArticle.scrollIntoView({ behavior: 'smooth' });
+	}
+
+	function handleClick(event) {
+		expandCard();
+		scrollIntoView(event);
+	}
+</script>
+
+<article
+	style="	{open ? `block-size: ${blockSize + 120}px` : ''}"
+	class="		
+		skillCard forcedClrAdjust
+		relative flex
+		h-[300px] flex-auto basis-[30ch] flex-col rounded-lg p-4 transition-[height] delay-[0s] duration-1000
+		[container-type:inline-size]
+		group-data-[themePalette=main]:outline-0
+		group-data-[themePalette=main]:focus-visible:outline-2
+		dark:bg-slate-1/12
+		dark:outline
+		dark:outline-primaryColor
+		dark:focus-visible:outline-dashed
+		dark:[&:is(:hover,:focus-visible)]:outline-[canvasText]
+		">
+	<!--  -->
+	<header data-theme="warning" class="mb-8-16 flex items-end gap-4">
+		<slot name="icon" />
+		<h3 class="mb-0 contrast-more:!text-[canvasText]">
+			{title}
+		</h3>
+	</header>
+
+	<div class="mb-7 overflow-hidden">
+		<div class="mb-[50px] contrast-more:!text-[canvasText]" bind:offsetHeight={blockSize}>
+			<slot />
+		</div>
+	</div>
+
+	<button on:click={handleClick} class=" outline-0">
+		<img
+			loading="lazy"
+			src={arrowDown}
+			alt="arrow down"
+			use:loadThemeBasedIcon={'arrowDown'}
+			class="{open
+				? 'bottom-4 rotate-180'
+				: 'animate-bounce'} duration-400 absolute left-[50cqw] bottom-1 transition-all" />
+	</button>
+</article>
+
+<style lang="postcss">
+</style>
