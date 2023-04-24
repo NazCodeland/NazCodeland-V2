2ff36cd80983188a139265c5f819a5e5d258bafb
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-04-24 10:59:47 -0400

    chore: implemt mobile view for Portfolio page

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -1,46 +1,76 @@
 <script lang="ts">
-	export let imageName: string = 'one.jpg';
+	export let imageName: string = 'nazCodeland.png';
 	export let project: string;
 	export let roles: string;
 	export let tools: string;
 	export let duration: string;
-	export let inlineSize: string = '270px';
-	export let blockSize: string = '520px';
+	export let inlineSize: string = '270';
+	export let blockSize: string = '520';
 	export let objectFit: string = 'cover';
 	export let objectPosition: string = 'top';
+
+	let showDesktop: boolean;
+
+	function switchImage() {
+		setTimeout(() => {
+			if (showDesktop) {
+				imageName = 'nazCodeland.png';
+			} else {
+				imageName = 'nazCodelandMobile.png';
+			}
+		}, 450);
+	}
+
+	function handleClick() {
+		showDesktop = !showDesktop;
+		switchImage();
+	}
 </script>
 
-<figure class="group/project relative overflow-hidden rounded-2xl border border-slate-600 p-1">
-	<a href="/portfolio/{project}">
+<figure
+	style="max-inline-size: {inlineSize}px; block-size: {blockSize}px;"
+	class="three-d-container group/project relative overflow-hidden rounded-2xl border border-slate-600 p-1 transition-all duration-200">
+	<a
+		href="/portfolio/{project}"
+		class="three-d-item {showDesktop
+			? '[--rotateY:180deg] [--translateX:-74px] [--translateZ:-60px]'
+			: ''} transition-all duration-1400">
 		<img
 			loading="lazy"
-			style="inline-size: {inlineSize}; block-size: {blockSize}; object-fit:{objectFit}; object-position:{objectPosition};"
+			style="inline-size: {inlineSize}px; block-size: {blockSize}px; object-fit:{objectFit}; object-position:{objectPosition};"
 			class="rounded-xl transition-[transform] delay-[0s] duration-200 hover:scale-[1.15]"
 			src="/images/{imageName}"
 			alt="a cute dog" />
-	</a>
-	<figcaption class="text-sm">
 		<span
-			class="absolute top-3 ml-2 mr-3 rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy outline outline-1 outline-current">
+			class="{showDesktop
+				? 'rotate-180'
+				: ''} projectT-title absolute top-7 ml-4 mr-3 transform rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy outline outline-1 outline-current">
 			<i>Project:</i>
 			{project}
 		</span>
+	</a>
+	<figcaption class="text-sm">
+		<!-- <span
+			class="absolute top-3 ml-2 mr-3 rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy outline outline-1 outline-current">
+			<i>Project:</i>
+			{project}
+		</span> -->
 		<span
-			class="absolute bottom-[68px] ml-2 mr-3 rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy
+			class="absolute bottom-[68px] mr-3 rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy
 			outline outline-1 outline-current
 			[@media(hover:hover)]:-left-[150%] [@media(hover:hover)]:transition-[left] [@media(hover:hover)]:delay-[0s] [@media(hover:hover)]:duration-300 [@media(hover:hover)]:group-hover/project:left-2">
 			<i>Roles:</i>
 			{roles}
 		</span>
 		<span
-			class="absolute bottom-10 ml-2 mr-3 rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy
+			class="absolute bottom-10 mr-3 rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy
 			outline outline-1 outline-current
 			[@media(hover:hover)]:-left-[150%] [@media(hover:hover)]:transition-[left] [@media(hover:hover)]:delay-200 [@media(hover:hover)]:duration-300 [@media(hover:hover)]:group-hover/project:left-2">
 			<i>Tools:</i>
 			{tools}
 		</span>
 		<span
-			class="absolute bottom-3 ml-2 mr-3 rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy
+			class="absolute bottom-3 mr-3 rounded-md bg-secondaryColor/85 py-0.5 px-2 text-bodyCopy
 			outline outline-1 outline-current
 			[@media(hover:hover)]:-left-[150%] [@media(hover:hover)]:transition-[left] [@media(hover:hover)]:delay-300 [@media(hover:hover)]:duration-300 [@media(hover:hover)]:group-hover/project:left-2">
 			<i>Duration:</i>
@@ -48,6 +78,31 @@
 		</span>
 	</figcaption>
 </figure>
+<div>
+	<button on:click={handleClick} class="rounded-md border px-4 py-1">
+		View {showDesktop ? 'mobile' : 'desktop'} version</button>
+</div>
 
 <style>
+	:root {
+		--rotateY: 0deg;
+		--translateZ: 60px;
+		--translateX: 0px;
+	}
+
+	.three-d-container {
+		display: grid;
+		perspective: 800px;
+		transform-style: preserve-3d;
+	}
+
+	.three-d-item {
+		transform: rotateY(var(--rotateY));
+		transform-style: preserve-3d;
+	}
+
+	.projectT-title {
+		transition-delay: 0.45s;
+		transform: translateZ(var(--translateZ)) rotateY(var(--rotateY)) translateX(var(--translateX));
+	}
 </style>
--- src/routes/(app)/portfolio/+page.svelte
+++ src/routes/(app)/portfolio/+page.svelte
@@ -1,10 +1,6 @@
-<script>
+<script lang="ts">
 	import Project from '$lib/components/Project.svelte';
 	import Link from '$lib/elements/InlineLink.svelte';
-	let prev;
-	let next;
-	let turnLeft;
-	let turnRight;
 </script>
 
 <!-- TODO: add a sorting navigation to sort by project roles-->
@@ -12,30 +8,54 @@
 <main class="m-auto flex h-[1800px] w-full max-w-[94ch] flex-col gap-56-72">
 	<h1>Latest Projects</h1>
 
-	<div class="project">
+	<div class="projects flex flex-col gap-8">
+		<!--  -->
 		<Project
-			imageName="nazcodeland.png"
+			imageName="nazCodelandMobile.png"
 			project="NazCodeland"
 			roles="User Interface / Front-end"
 			tools="Pen & Paper / Miro / Figma"
-			duration="4 Months" />
+			duration="4 Months"
+			inlineSize="600" />
 
 		<Project
-			imageName="nazcodelandMobile.png"
+			imageName="nazCodelandMobile.png"
 			project="PawFood"
 			roles="User Research / User Interface / Front-end / Back-end"
 			tools="Pen & Paper / Miro / Figma"
 			duration="4 Months" />
 
 		<Project imageName="PawFood.jpg" project="THE FUTURE" roles="--" tools="--" duration="--" />
+
+		<!--  -->
 	</div>
 </main>
 
 <style>
+	/* :root {
+		--rotateY: 0deg;
+	}
+
+	.scene {
+		perspective: 800px;
+		transform-style: preserve-3d;
+	}
+
 	.project {
+		background-color: var(--bgClr);
+		transform: rotateY(var(--rotateY));
+	}
+
+	.projects {
+		border: 2px solid blue;
+		display: flex;
+		flex-direction: column;
+		gap: 32px;
+		align-items: center;
+		flex-grow: 1;
 		place-items: center;
 		gap: 32px;
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
-	}
+	} */
 </style>
